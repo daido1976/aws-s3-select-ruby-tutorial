9b95506f23a48f9d1876afe1bf02799c83aeb2a5
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-04-03 06:53:26 +0900

    Refactor generate csv with lambda

--- generate_csv.rb
+++ generate_csv.rb
@@ -1,10 +1,20 @@
 require 'csv'
 require 'ffaker'
 
+schema = lambda do |id = nil|
+  {
+    id: id,
+    name: FFaker::NameJA.name,
+    email: FFaker::Internet.email,
+    address: FFaker::AddressJA.address,
+    job: FFaker::JobJA.title
+  }
+end
+
 # Before execute $ mkdir tmp
 CSV.open('tmp/test.csv', 'wb') do |csv|
-  csv << %i[id name email address job]
+  csv << schema.call.keys
   10_000.times do |n|
-    csv << [n, FFaker::NameJA.name, FFaker::Internet.email, FFaker::AddressJA.address, FFaker::JobJA.title]
+    csv << schema.call(n).values
   end
 end
